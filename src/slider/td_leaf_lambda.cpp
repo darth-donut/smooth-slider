9de93057d27730dd30e9b9a1eb65ed4b5c5bf8d1
jfong <jfong361@gmail.com>
2017-12-09 13:07:46 +1100

    fix calculatino

--- src/slider/td_leaf_lambda.cpp
+++ src/slider/td_leaf_lambda.cpp
@@ -3,12 +3,12 @@
 // @date  : 9/12/17 4:49 AM
 //
 
+#include <vector>
+
 #include "td_leaf_lambda.h"
 
-double
-sech
 
-TDLeafLambda::TDLeafLambda(Model& model,
+TDLeafLambda::TDLeafLambda(Model &model,
                            std::mutex &model_lock,
                            const std::vector<Move> &move_history)
         : model(model),
@@ -20,7 +20,17 @@ TDLeafLambda::update_weights() {
     constexpr double alpha = 1.0;
     constexpr double lambda = .7;
     // todo: tune this to suitable values for a and b
-    constexpr double a  = 1, b = 1;
+    constexpr double a = 1, b = 1;
+    std::vector<double> lambda_array;
+
+    for (int t = 0; t < move_history.size() - 1; ++t) {
+        double t_lambda_sum = 0;
+        for (int i = t; i < move_history.size(); ++i) {
+            t_lambda_sum += pow(lambda, i - t) *
+                               (move_history[i + 1].get_metadata().second - move_history[i].get_metadata().second);
+        }
+        lambda_array.push_back(t_lambda_sum);
+    }
 
     // for each weight:
     for (int k = 0; k < model.size(); ++k) {
@@ -28,17 +38,11 @@ TDLeafLambda::update_weights() {
         // where d_j = move_history[j+1].second - meta.second (i.e. difference between immediate state and current)
         // SIG(t = 1, N - 1): FOR ALL STATES
         double delta_weight = 0;
-        for (int i = 0; i < move_history.size() - 1; ++i) {
+        for (int t = 0; t < move_history.size() - 1; ++t) {
             // meta.first = state, meta.second = eval value
-            auto meta = move_history[i].get_metadata();
-            // difference in score between state i+1 and i multiplied by lambda
-            double lambda_d_j = 0;
-            // SIG(i = t, N - 1): FOR ALL STATES FROM INDEX t ONWARDS
-            for (int j = i; j < move_history.size(); ++j) {
-                lambda_d_j += pow(lambda, j - i) * (move_history[j+1].get_metadata().second - meta.second);
-            }
+            auto meta = move_history[t].get_metadata();
             // todo: sech2 and phi
-            delta_weight += alpha * a * b * sech2(b * meta.second) * phi[i](meta.first) * lambda_d_j;
+            delta_weight += alpha * a * b * sech2(b * meta.second) * phi[k](*meta.first) * lambda_array[t];
         }
         model[k] += delta_weight;
     }
